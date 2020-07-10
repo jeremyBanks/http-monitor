74c82e828ce4985f70ad38b6a71fc260c5e4dadd
Jeremy Banks <_@jeremy.ca>
2020-07-10 23:38:40 +0000

    r2 six echo four alfa

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
+use anyhow::{anyhow, Context, Result};
+use argh;
+use csv;
+use serde::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 use std::{
     borrow::Cow,
-    io::{stdin, stdout, stderr}
+    io::{Read, Write, stdin, stdout},
 };
-use anyhow::{Context, Result};
-use serde::Deserialize;
-use serde_derive::Deserialize;
-use csv;
 
-#[derive(Debug, Deserialize)]
-struct LogEntry {
-    #[serde(rename="remotehost")]
+#[derive(argh::FromArgs)]
+/// Generates alerts to stdout from HTTP logs via stdin.
+struct Args {}
+
+#[derive(Debug, Deserialize, Serialize, Clone, Ord, PartialOrd, Eq, PartialEq)]
+struct RequestRecord {
+    #[serde(rename = "remotehost")]
     remote_host: String,
     rfc931: String,
-    #[serde(rename="authuser")]
+    #[serde(rename = "authuser")]
     auth_user: String,
     date: u64,
     request: String,
@@ -21,19 +26,37 @@ struct LogEntry {
     population: Option<u64>,
 }
 
-#[derive(Debug, Deserialize)]
-struct Record {
-    city: String,
-    region: String,
-    country: String,
-    population: Option<u64>,
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct MonitorConfig {
+
 }
 
-fn main() -> Result<()>  {
-    let mut reader = csv::Reader::from_reader(stdin());
-    for result in reader.deserialize() {
-        let record: LogEntry = result?;
-        println!("{:?}", record);
+fn main() -> Result<()> {
+    if atty::is(atty::Stream::Stdin) {
+        eprintln!("ERROR: stdin must be a stream or file, not a terminal.");
+        eprintln!();
+        eprintln!("example usage:");
+        eprintln!("    cargo run < sample_input.csv");
+        eprintln!();
+        eprintln!("or with a release binary:");
+        eprintln!("    cargo build --release");
+        eprintln!("    target/release/dd-monitor < sample_input.csv");
+
+        std::process::exit(1)
     }
+
+    monitor(&mut stdin(), &mut stdout(), &MonitorConfig{})
+}
+
+fn monitor(source: &mut impl Read, sink: &mut impl Write, _config: &MonitorConfig) -> Result<()> {
+    let mut reader = csv::Reader::from_reader(source);
+
+    let previous: Option<RequestRecord> = None;
+
+    for result in reader.deserialize() {
+        let record: RequestRecord = result?;
+        writeln!(sink, "{}", record.remote_host)?;
+    }
+
     Ok(())
 }
